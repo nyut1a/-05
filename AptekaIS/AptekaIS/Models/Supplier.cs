995db5a7a4b16839be8bfeb50c16b422f0fe683d
anna maurina <anya_maurina@mail.ru>
2026-05-31 03:00:35 +0300

    first commit

--- AptekaIS/AptekaIS/Models/Supplier.cs
+++ AptekaIS/AptekaIS/Models/Supplier.cs
@@ -3,7 +3,8 @@ namespace AptekaIS.Models;
 public class Supplier
 {
     public int Id { get; set; }
-    public string Name { get; set; } = "";
-    public string Phone { get; set; } = "";
-    public string Address { get; set; } = "";
+    public string CompanyName { get; set; } = "";
+    public string INN { get; set; } = "";
+    public string ContactPhone { get; set; } = "";
+    public ICollection<Batch> Batches { get; set; } = new List<Batch>();
 }
